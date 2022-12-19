82a676aa343d088aa6acdecbe0c36591b6316944
Vasili Karpenkin <v.karpenkin@gmail.com>
2022-12-19 23:41:44 +0300

    19.12.2022

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React, {useState} from "react";
+import './App.scss';
 
 function App() {
+    const [visiblePopup, setVisiblePopup] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Песочница</div>
-          <p>Курс: Front-end Разработчик</p>
+      <header className="Header">
+        <h1>Sandbox</h1>
+          <button onClick={()=>setVisiblePopup(!visiblePopup)}>
+              {visiblePopup ? 'Close' : 'Open'}
+          </button>
+          {visiblePopup &&
+              <div className="Info">
+                  <p>Karpenkin Vasili</p>
+                  <p>Course: Front-end Developer</p>
+              </div>}
       </header>
     </div>
   );
