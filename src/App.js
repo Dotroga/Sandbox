dc584faae065192f1364d1f3d9cd74e774fa7063
Vasili Karpenkin <v.karpenkin@gmail.com>
2022-12-20 23:06:40 +0300

    20.12.2022 №1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, {useState} from "react";
+import BackgroundColor from "./copmonents/BackgroundColor";
 import './App.scss';
 
 function App() {
     const [visiblePopup, setVisiblePopup] = useState(false)
+    console.log(document.body.style.backgroundColor)
   return (
-    <div className="App">
-      <header className="Header">
+    <div className="app">
+        <div className='color'>
+            <BackgroundColor/>
+        </div>
+      <header className="header">
         <h1>Sandbox</h1>
           <button onClick={()=>setVisiblePopup(!visiblePopup)}>
               {visiblePopup ? 'Close' : 'Open'}
           </button>
           {visiblePopup &&
-              <div className="Info">
+              <div className="info">
                   <p>Karpenkin Vasili</p>
                   <p>Course: Front-end Developer</p>
               </div>}
---
+++ src/copmonents/BackgroundColor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BackgroundColor = () => {
+    document.body.style.backgroundColor = "#AA0324";
+    return (<button>
+            Change color
+        </button>)
+}
+export default BackgroundColor;
