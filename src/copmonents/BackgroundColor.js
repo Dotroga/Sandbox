5cae7991e7363aecb0f82228b6b9ff30301bb2cc
Vasili Karpenkin <v.karpenkin@gmail.com>
2022-12-21 18:25:44 +0300

    21.12.2022

--- src/copmonents/BackgroundColor.js
+++ src/copmonents/BackgroundColor.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import hex from './../Hex'
+import './BackgroundColor.scss'
 
 const BackgroundColor = () => {
+    const [hexColor, setHexColor] = useState()
+
     const changeColor = () => {
         document.body.style.backgroundColor = generateHex()
     }
+
     const generateHex = () => {
         let hexColor = '#'
         for(let i=0;i<6;i++) {
             hexColor += hex[Math.floor((Math.random() * hex.length))]
         }
+        setHexColor(hexColor)
         return hexColor
     }
-    return (<button onClick={()=>changeColor()}>
-            Change color
-        </button>)
+
+    return (
+        <div >
+            <button className='changeColor'
+                    onClick={()=>changeColor()}>
+                Change color
+            </button>
+            <div className='hexColor'>
+                {hexColor}
+            </div>
+        </div>)
 }
 export default BackgroundColor;
